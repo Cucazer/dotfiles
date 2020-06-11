e9e4d231b76c9ed45bf92262f02e372c5bfd3269
Konstantins Franckevics <cucazik@inbox.lv>
2020-06-12 01:14:31 +0200

    Replace settings.json if exists

--- apply_config.sh
+++ apply_config.sh
@@ -2,7 +2,7 @@
 
 cat .vimrc | tee -a ~/.vimrc
 
-cat settings.json | tee "~/.config/Code - OSS/User/settings.json"
+cp -f settings.json "${HOME}/.config/Code - OSS/User/settings.json"
 
 cat config.fish | tee -a ~/.config/fish/config.fish
 cat aliases.fish| tee -a ~/.config/fish/aliases.fish
