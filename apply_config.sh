dfe6452c6c261d0853bab32811c29f876fb69b16
Cucazer <cucazik@inbox.lv>
2020-07-25 17:39:47 +0200

    Merge existing config files manually

--- apply_config.sh
+++ apply_config.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-cat .vimrc | tee -a ~/.vimrc
+update_file () {
+    source=$1
+    target=$2
+    echo
+    echo "$source               >               $target"
+    echo
+    touch "$target"
+    sdiff -o temp.txt "$source" "$target"
+    mv temp.txt "$target"
+}
 
-cp -f settings.json "${HOME}/.config/Code - OSS/User/settings.json"
+update_file .vimrc ~/.vimrc
 
-cat config.fish | tee -a ~/.config/fish/config.fish
-cat aliases.fish| tee -a ~/.config/fish/aliases.fish
+if [ -d "${HOME}/.config/Code - OSS" ]
+then
+    update_file settings.json "${HOME}/.config/Code - OSS/User/settings.json"
+fi
+
+if [ -d ~/.config/fish ]
+then
+    update_file config.fish ~/.config/fish/config.fish
+    update_file aliases.fish ~/.config/fish/aliases.fish
+fi
